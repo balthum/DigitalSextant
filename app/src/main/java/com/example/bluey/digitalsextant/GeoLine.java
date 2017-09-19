f0c9944df552dafb1537f5fc517e5176ec376ced
toddgibson <balthum0@gmail.com>
2017-09-19 16:46:21 -0700

    Discreption of Geographic Line

---
+++ app/src/main/java/com/example/bluey/digitalsextant/GeoLine.java
@@ -0,0 +1,35 @@
+package com.example.bluey.digitalsextant;
+
+/**
+ * Created by toddgibson Todd Gibson on 9/19/17.
+ */
+
+public class GeoLine
+{
+    GeoPosition PositionOne;
+    GeoPosition PositionTwo;
+
+    public GeoLine()
+    {
+    }
+
+    public GeoPosition getPositionOne()
+    {
+        return PositionOne;
+    }
+
+    public void setPositionOne(GeoPosition positionOne)
+    {
+        PositionOne = positionOne;
+    }
+
+    public GeoPosition getPositionTwo()
+    {
+        return PositionTwo;
+    }
+
+    public void setPositionTwo(GeoPosition positionTwo)
+    {
+        PositionTwo = positionTwo;
+    }
+}
